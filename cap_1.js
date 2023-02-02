3091c09f8077ca2702fd0495905342c029329983
Vivian De Mitry <107444675+viviandemitry@users.noreply.github.com>
2023-02-02 20:46:20 -0300

    Create cap_1.js
    Anotações do primeiro capítulo do livro.

---
+++ cap_1.js
@@ -0,0 +1,53 @@
+//Estruturas de controle
+
+//Instruções condicionais são tratadas com if...else e switch.Laços são tratados com as contruções while, do...while e for.
+
+//exemplos:
+
+if (num === 1) {
+  num--;
+ } else {
+  num++;
+  }
+  
+  //ou:
+  
+  (num === 1) ? num-- : num++;
+  
+  //Laços
+  
+  //Exemplos de como exibir valores entre 0 a 9 no console com diferentes funções
+  
+  
+  //ex1
+  
+  for (var i = 0; i<10; i++) {
+  console.log(i);
+  }
+  
+ //ex2
+
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+//ex3
+
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
+
+
+
+
+
+
+
+
+
+
+
