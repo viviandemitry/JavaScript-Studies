41b0878cdafbdd78495b230207a6d222560b8687
Vivian De Mitry <107444675+viviandemitry@users.noreply.github.com>
2023-02-13 14:29:35 -0300

    Update course.js

--- JavaScript Algorithms and Data Structures Masterclass/course.js
+++ JavaScript Algorithms and Data Structures Masterclass/course.js
@@ -1,1 +1,25 @@
 
+//EX1: Suppose we want to write a function that calculates the sum of all number from 1 up to (and including) some number n.
+
+function addUpTo(n) {
+  let total = 0;
+  for (let i=1; i<= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+//or
+
+function addUpTo(n) {
+  return n * (n + 1) / 2;
+}
+
+//ex2
+
+function logAtMost5 (n) {
+  for (var i = 1; i <= Math.min(5, n); i++) {
+    console.log(i);
+  }
+} // resultado: menor ou igual a 5.
+
