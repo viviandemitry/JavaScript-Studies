7adc2b7c8edc90696a9a05679831eb09121d3e26
Vivian De Mitry <107444675+viviandemitry@users.noreply.github.com>
2023-02-13 21:17:12 -0300

    Update exercises.js

--- JavaScript Algorithms and Data Structures Masterclass/exercises.js
+++ JavaScript Algorithms and Data Structures Masterclass/exercises.js
@@ -23,3 +23,15 @@ function logAtMost5 (n) {
   }
 } // resultado: menor ou igual a 5.
 
+//UNDERTAND THE PROBLEM
+
+//1. Can I restate the problem in my own words?
+//2. What are the inputs that go into the problem?
+//3. What are the outputs that should come from the solution to the problem?
+//4. Can the outputs be determined from the inputs?
+//5. How sould I label the important pieces of data that are a part of the problem?
+
+//ex: Write a function hich takes in a string and returns counts of each character in the string.
+
+charCount("aaaa"); //{a:4}
+charCount("hello"); // {h1:1, e:1, l:2, o:1}
